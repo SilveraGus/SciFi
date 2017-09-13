1bd00c09976cc05a8cb249b40505d777a6a07f84
GustavoSilvera <gustavo.r.silvera@gmail.com>
2017-09-12 20:26:18 -0700

    https://github.com/SilveraGus/SciFi

---
+++ SciFiName.java
@@ -0,0 +1,56 @@
+// generate a sciFi name
+
+
+/**
+ * SciFiName gathers input from a user to generate a custom name that
+ * sounds like it could be used in a science fiction book or movie.
+ * 
+ * @author ckinnard
+ * @version 3/4/16
+ */
+
+
+public class SciFiName   
+{
+    /**
+     * All input that you privide should be at least three letters long
+     * or the program may crash (but feel free to test this!)
+     * 
+     * For best results, user lowercase letters and do not use spaces in your input
+     */
+    public static void main()
+    {
+        System.out.println("If you provide me some information I will provide a Science Fiction name for you.");
+        System.out.println("Please have all responses  be at least three characters long.");
+        System.out.println("For best results, user lowercase letters with no spaces.\n");
+
+
+        // notice this methd (print) does not print a newline
+        System.out.print("Enter your first name: ");
+        String firstName = UserInput.getString();
+        System.out.print("Enter your last name: ");
+        String lastName = UserInput.getString();
+        System.out.print("Enter the city where you or one of your parents were born: ");
+        String city = UserInput.getString();
+        System.out.print("Enter the name of your grammar school: ");
+        String school = UserInput.getString();
+        System.out.print("Enter the first name of a sibling or other relative: ");
+        String relativeName1 = UserInput.getString();
+        System.out.print("Enter the first name of a second sibling or relative: ");
+        String relativeName2 = UserInput.getString();
+
+
+        // generate a sciFi name
+        System.out.println("the good stuffs:");
+        String name = (firstName.substring(0, 3) + lastName.substring(0, 2));
+        String lname =(city.substring(0, 2) + school.substring(0, 3));
+        //System.out.println(city.substring(0, 2) + school.substring(0, 2));
+        double num = Math.random() * (firstName.length()-1) + 1;
+        String s1 = (relativeName1.substring((int)num, relativeName1.length()));
+        double num2 = Math.random() * (lastName.length()-1) + 1;
+        String s2 = (relativeName2.substring((int)num2, relativeName2.length()));
+        System.out.println(s1+s2);
+        System.out.println("Hello "+ name + " " + lname + " of " + s1 + s2 + ". Welcome!");
+        
+    }
+}
